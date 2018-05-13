4b6078e46a3d929635140a5d515e207798a62c0a
Tian0820 <151250121@smail.nju.edu.cn>
2018-05-13 12:30:53 +0800

    <dev>:<modify package.json (add lean cloud dependencies)>

--- index.js
+++ index.js
@@ -4,15 +4,28 @@ const router = require('koa-router')(); // 引入各种依赖
 const path =require('path')
     , serve = require('koa-static');
 
+const AV = require('leanengine');
+
+AV.init({
+    appId: process.env.LEANCLOUD_APP_ID || 'GLd1iHuvtxpzpWilAoasYC57-gzGzoHsz',
+    appKey: process.env.LEANCLOUD_APP_KEY || '3P54ey1zeTwTEXmmDeKCpr0H',
+    masterKey: process.env.LEANCLOUD_APP_MASTER_KEY || 'rQuB50Lo92FJD9es4KHTQC9x'
+});
+
 const app = new Koa();
 
 app.use(require('koa-bodyparser')());
+app.use(AV.koa());
+app.listen(process.env.LEANCLOUD_APP_PORT);
 
-app.use(function* (next){
-    let start = new Date;
-    yield next;
-    let ms = new Date - start;
-    console.log('%s %s - %s', this.method, this.url, ms); // 显示执行的时间
+app.use(function *(next) {
+    if (this.url === '/todos') {
+        return new AV.Query('Todo').find().then(todos => {
+            this.body = todos;
+        });
+    } else {
+        yield next;
+    }
 });
 
 app.use(serve(path.resolve('public')));
@@ -21,8 +34,8 @@ app.on('error', function(err, ctx){
     console.log('server error', err);
 });
 
-app.listen(8889,() => {
-    console.log('Koa is listening in 8889');
-});
+// app.listen(8889,() => {
+//     console.log('Koa is listening in 8889');
+// });
 
 module.exports = app;
