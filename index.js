622cb3df1c4857517d838fafe93fbb5c162219f2
Tian0820 <151250121@smail.nju.edu.cn>
2018-05-13 11:54:09 +0800

    <dev>:<modify static resource / modify the koa configurations>

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 const Koa = require('koa');
+const router = require('koa-router')(); // 引入各种依赖
+
+const path =require('path')
+    , serve = require('koa-static');
+
 const app = new Koa();
 
-const main = ctx => {
-  ctx.response.body = 'Hello World';
-};
+app.use(require('koa-bodyparser')());
 
-app.use(main);
-app.listen(3000);
+app.use(function* (next){
+    let start = new Date;
+    yield next;
+    let ms = new Date - start;
+    console.log('%s %s - %s', this.method, this.url, ms); // 显示执行的时间
+});
+
+app.use(serve(path.resolve('public')));
+
+app.on('error', function(err, ctx){
+    console.log('server error', err);
+});
+
+app.listen(8889,() => {
+    console.log('Koa is listening in 8889');
+});
+
+module.exports = app;
