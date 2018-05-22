c39c4380f0419885ee05ed1886708469166cb97e
Tian0820 <151250121@smail.nju.edu.cn>
2018-05-22 17:25:49 +0800

    <dev>:<modify the static resource>

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,20 @@
 const router = require('koa-router')()
 const fs = require('fs')
 
-router.prefix('/user')
+router.prefix('/api/user')
 
 router.get('/get', async function (ctx) {
-    console.log(ctx.query)
-    let data = fs.readFileSync('./data/user.json', 'utf8')
-    ctx.body = JSON.parse(data);
+    console.log(ctx.query);
+    let name = ctx.query.name;
+    let data = JSON.parse(fs.readFileSync('./data/user.json', 'utf8'));
+    let info = null;
+
+    data.users.forEach(user => {
+        if(user.name === name) {
+            info = user;
+        }
+    })
+    ctx.body = info
 
 });
 
