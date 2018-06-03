bcfeb4679826dbe8e61e067fd95fc5e0f4692409
Tian0820 <151250121@smail.nju.edu.cn>
2018-06-03 16:23:28 +0800

    <dev>:<add update and delete methods>

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,42 @@
 const router = require('koa-router')()
 const fs = require('fs')
+const database = require('../lib/database')
 
 router.prefix('/api/user')
 
 router.get('/get', async function (ctx) {
-    console.log(ctx.query);
-    let name = ctx.query.name;
-    let data = JSON.parse(fs.readFileSync('./data/user.json', 'utf8'));
-    let info = null;
-
-    data.users.forEach(user => {
-        if(user.name === name) {
-            info = user;
-        }
-    })
+    let info = await database.getByName(ctx.query.name)
     ctx.body = info
-
 });
 
+router.get('/getAll', async function (ctx) {
+    let info = await database.getAll()
+    ctx.body = info
+});
+
+router.post('/delete', async function (ctx) {
+    let name = ctx.request.body.name
+    let friendToDelete = await database.getByName(name)
+    let info = await database.deleteById(friendToDelete[0].id)
+    ctx.body = {
+        result: 'success',
+        message: '删除成功！'
+    }
+})
+
+router.post('/update', async function (ctx) {
+    let name = ctx.request.body.name
+    let friendToUpdate = await database.getByName(name)
+
+    let info = await database.updateFriend(ctx.request.body.name, ctx.request.body.sex, ctx.request.body.tags, friendToUpdate[0].id)
+    ctx.body = {
+        result: 'success',
+        message: '更新成功！'
+    }
+})
+
 router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
+    ctx.body = 'this is a users/bar response'
 })
 
 module.exports = router
